67f7b44dfa139ba56e24cf0b48001487fddfb0a9
Wenxuan Zhang <wenxuangm@gmail.com>
2021-03-19 15:58:11 +0800

    refactor: improve error handling

--- src/core.rs
+++ src/core.rs
@@ -1,50 +1,32 @@
-use csv::{self, ErrorKind, ReaderBuilder, StringRecord};
+use csv::{self, ReaderBuilder, StringRecord};
 use prettytable::{format, Cell, Row, Table};
 use std::io::BufRead;
-use std::process;
 
-pub fn print_csv(reader: impl BufRead, has_headers: bool, delimiter: char, style: format::TableFormat) {
+pub fn print(
+    reader: impl BufRead,
+    has_headers: bool,
+    delimiter: char,
+    style: format::TableFormat,
+) -> anyhow::Result<()> {
     let csv_reader = &mut ReaderBuilder::new()
         .delimiter(delimiter as u8)
         .has_headers(has_headers)
         .from_reader(reader);
-    let mut table = Table::init(csv_reader.records().map(record_to_row).collect());
+    let rows: Vec<_> = csv_reader
+        .records()
+        .map(|r| r.map(|r| record_to_row(&r)))
+        .collect::<csv::Result<_>>()?;
+    let mut table = Table::init(rows);
 
     if has_headers {
-        table.set_titles(record_to_row(csv_reader.headers().map(|r| r.to_owned())));
+        let header = csv_reader.headers()?;
+        table.set_titles(record_to_row(header));
     }
     table.set_format(style);
-    table.printstd();
+    table.print_tty(false)?;
+    Ok(())
 }
 
-fn record_to_row(record: csv::Result<StringRecord>) -> Row {
-    match record {
-        Ok(record) => Row::new(record.into_iter().map(|cell| Cell::new(&cell)).collect()),
-        Err(error) => match error.kind() {
-            ErrorKind::Utf8 { .. } => {
-                eprintln!("[error] input is not utf8 encoded");
-                process::exit(exitcode::DATAERR)
-            }
-            ErrorKind::UnequalLengths { pos, expected_len, len } => {
-                let pos_info = if let Some(p) = pos {
-                    format!(" at (byte: {}, line: {}, record: {})", p.byte(), p.line(), p.record())
-                } else {
-                    "".to_string()
-                };
-                eprintln!(
-                    "[error] unequal lengths{}: expected length is {}, but got {}",
-                    pos_info, expected_len, len
-                );
-                process::exit(exitcode::DATAERR)
-            }
-            ErrorKind::Io(e) => {
-                eprintln!("[error] io error: {}", e);
-                process::exit(exitcode::IOERR)
-            }
-            e => {
-                eprintln!("[error] failed to process input: {:?}", e);
-                process::exit(exitcode::SOFTWARE)
-            }
-        },
-    }
+fn record_to_row(record: &StringRecord) -> Row {
+    Row::new(record.iter().map(|cell| Cell::new(cell)).collect())
 }
--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,47 @@ mod cli;
 mod core;
 mod style;
 use cli::{CompletionOpt, Opt, StructOpt, Subcommand};
+use csv::ErrorKind;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
+use std::process;
 
 fn main() {
     if let Err(e) = try_main() {
         if let Some(ioerr) = e.root_cause().downcast_ref::<io::Error>() {
             if ioerr.kind() == io::ErrorKind::BrokenPipe {
-                std::process::exit(0);
+                process::exit(0);
             }
         }
+
+        if let Some(csverr) = e.root_cause().downcast_ref::<csv::Error>() {
+            match csverr.kind() {
+                ErrorKind::Utf8 { .. } => {
+                    eprintln!("[error] input is not utf8 encoded");
+                    process::exit(exitcode::DATAERR)
+                }
+                ErrorKind::UnequalLengths { pos, expected_len, len } => {
+                    let pos_info = pos
+                        .as_ref()
+                        .map(|p| format!(" at (byte: {}, line: {}, record: {})", p.byte(), p.line(), p.record()))
+                        .unwrap_or_else(|| "".to_string());
+                    eprintln!(
+                        "[error] unequal lengths{}: expected length is {}, but got {}",
+                        pos_info, expected_len, len
+                    );
+                    process::exit(exitcode::DATAERR)
+                }
+                ErrorKind::Io(e) => {
+                    eprintln!("[error] io error: {}", e);
+                    process::exit(exitcode::IOERR)
+                }
+                e => {
+                    eprintln!("[error] failed to process input: {:?}", e);
+                    process::exit(exitcode::SOFTWARE)
+                }
+            }
+        }
+
         eprintln!("{}: {}", env!("CARGO_PKG_NAME"), e);
         std::process::exit(1)
     }
@@ -29,7 +60,7 @@ fn try_main() -> anyhow::Result<()> {
                 None => Box::new(BufReader::new(io::stdin())),
             };
             let delimiter = if opt.tsv { '\t' } else { opt.delimiter };
-            core::print_csv(reader, !opt.no_headers, delimiter, opt.border.into());
+            core::print(reader, !opt.no_headers, delimiter, opt.border.into())?;
         }
     }
     Ok(())
